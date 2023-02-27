3f39a7ea2e2cccbe4b7fcf95109cc1fb1a614df4
1kivanc <kivanc0556@gmail.com>
2023-02-27 15:06:32 +0300

    session

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
+var session = require('express-session');
 const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
 const categoryRoute = require("./routes/categoryRoute");
@@ -21,17 +21,32 @@ mongoose.connect("mongodb://127.0.0.1:27017/smartedu-db", {
 
 app.set("view engine", "ejs");
 
+//Global variables
+global.userIN = null;
+
 //Middlewares
 
 app.use(express.static("public"));
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+app.use(session({
+  secret:'my_keyboard_cat',
+  resave:false,
+  saveUninitialized: true,
+  
+}))
+
 //Routes
+app.use('*',(req,res,next)=> {
+  userIN = req.session.userID;
+  next();
+})
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute);
 app.use("/users", userRoute);
+
 
 const port = 3000;
 app.listen(port, () => {
