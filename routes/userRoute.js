9cfec729a2c0882a19054e94ef19b54b434cddf3
1kivanc <kivanc0556@gmail.com>
2023-02-27 16:34:54 +0300

    user dashboard

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,5 +7,7 @@ router.route('/signup').post(authController.createUser); // localhost:3000/cours
 // router.route('/yeni').post(courseController.yenikurs); // localhost:3000/courses/yenikurs
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
+router.route('/dashboard').get(authController.getDashboardPage);
+
 
 module.exports = router;
