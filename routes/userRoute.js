b577fcebc43214e2f0ae5d43ec2596ef4d0f2d67
1kivanc <kivanc0556@gmail.com>
2023-02-28 15:52:15 +0300

    custom middleware

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
@@ -7,7 +8,7 @@ router.route('/signup').post(authController.createUser); // localhost:3000/cours
 // router.route('/yeni').post(courseController.yenikurs); // localhost:3000/courses/yenikurs
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
-router.route('/dashboard').get(authController.getDashboardPage);
+router.route('/dashboard').get(authMiddleware,authController.getDashboardPage);
 
 
 module.exports = router;
