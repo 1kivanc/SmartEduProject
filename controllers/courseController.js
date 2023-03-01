2086ee5de7bc350d7034f032217917395cf45774
1kivanc <kivanc0556@gmail.com>
2023-03-01 15:41:40 +0300

    course&&teacher

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -30,7 +30,12 @@ exports.getAllCourses = async (req, res) => {
 exports.createCourse = async (req, res) => {
   try {
   
-  const course = await Course.create(req.body);
+  const course = await Course.create({
+    name:req.body.name,
+    description: req.body.description,
+    category:req.body.category,
+    user:req.session.userID
+  });
 
     res.status(201).redirect('/courses');
   } catch(error) {
@@ -44,7 +49,7 @@ exports.createCourse = async (req, res) => {
 exports.getCourse = async (req, res) => {
   
   try {
-    const course = await  Course.findOne({slug: req.params.slug})
+    const course = await  Course.findOne({slug: req.params.slug}).populate('user');
     res.status(200).render('course',{
       course,
       page_name:'course',
