7d88568f566418831997ec9c11b0e865b0b431ef
1kivanc <kivanc0556@gmail.com>
2023-03-05 11:38:18 +0300

    delete course and fix the ero

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -8,7 +8,9 @@ router.route('/').post(roleMiddleware(["teacher","admin"]),courseController.crea
 // router.route('/yeni').post(courseController.yenikurs); // localhost:3000/courses/yenikurs
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
+router.route('/:slug').delete(courseController.deleteCourse);
 router.route('/enroll').post(courseController.enrollCourse);
 router.route('/release').post(courseController.releaseCourse);
+
 
 module.exports = router;
