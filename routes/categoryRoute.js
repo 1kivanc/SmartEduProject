4d5d4b32e725a1da3603ae4eb2bbf411e520d6d4
1kivanc <kivanc0556@gmail.com>
2023-03-06 22:12:50 +0300

    deleteAndUpdateCategory

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.route('/').post(categoryController.createCategory); // localhost:3000/courses
 // router.route('/yeni').post(courseController.yenikurs); // localhost:3000/courses/yenikurs
+router.route('/:id').delete(categoryController.deleteCategory);
 
 
 module.exports = router;
